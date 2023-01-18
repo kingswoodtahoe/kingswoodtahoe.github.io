e9d259bf93680bfa161790d6ac20531999dbff9d
Adam Hull <adam@hmlad.com>
2023-01-18 10:43:42 -0800

    Minify built assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,23 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+import purgecss from "astro-purgecss";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    compress({
+      html: {
+        // Avoid problems with proxies
+        // https://stackoverflow.com/a/32684034
+        maxLineLength: 500,
+      },
+    }),
+    purgecss(),
+  ],
   site: "https://kingswoodtahoe.org",
 });
